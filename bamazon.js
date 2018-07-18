94d49afe4df009e2754c1c63ad9cb27e59f9662c
auztin410 <auztin410@gmail.com>
2018-07-18 08:29:29 -0400

    Successfully reducing stock from quantity ordered!

--- bamazon.js
+++ bamazon.js
@@ -28,38 +28,51 @@ function displayProducts(){
     connection.query("SELECT * FROM products", function(err, res){
         if(err) return console.log(err.message);
 
-        console.table(res);
-        orderItem();
+        // console.table(res);
+        // console.log(res);
+        console.log(res[0].stock);
+        inquirer.prompt([
+            {
+                name: "itemId",
+                input: "text",
+                message: "Please select an item's ID number to order."
+            },
+            {
+                name: "itemQuantity",
+                input: "text",
+                message: "How many would you like?"
+            }
+        ]).then(function(answer){
+            
+
+            var chosenItem;
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].id == answer.itemId) {
+                  chosenItem = res[i];
+                }
+            }
+            console.log(chosenItem);
+            var updateStock = chosenItem.stock -= answer.itemQuantity;
+            console.log(updateStock);
+            var query = connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock: updateStock
+                    },
+                    {
+                        id: answer.itemId
+                    }
+                ]
+            )
+        });
         // connection.end();
     })
 }
 
 function orderItem(){
 // Item ordering prompt.
-inquirer.prompt([
-    {
-        name: "itemId",
-        input: "text",
-        message: "Please select an item's ID number to order."
-    },
-    {
-        name: "itemQuantity",
-        input: "text",
-        message: "How many would you like?"
-    }
-]).then(function(answer){
-    var query = connection.query(
-        "UPDATE products SET ? WHERE ?",
-        [
-            {
-                stock: 50
-            },
-            {
-                id: answer.itemId
-            }
-        ]
-    )
-});
+
 
 }
 
